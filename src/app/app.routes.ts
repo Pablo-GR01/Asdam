586617e9f58578a9984b82de02b8b568979c627f
PabloGR <pablogr@MacBook-Air-2.local>
2025-07-15 19:16:51 +0200

    Étape 1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { Accueil } from '../page-User/accueil/accueil';
+
+
+export const routes: Routes = [
+    {path: 'accueil', component: Accueil}
+];
---
+++ src/page-User/accueil/accueil.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from "../../component-User/header/header";
+
+
+@Component({
+  selector: 'app-accueil',
+  imports: [Header],
+  templateUrl: './accueil.html',
+  styleUrl: './accueil.css'
+})
+export class Accueil {
+
+}
